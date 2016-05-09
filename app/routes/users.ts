0e73eb9a66890b0ce43602fcf0f4d98d99bc3f09
MikeyBurkman <michael.burkman@gmail.com>
2016-05-09 15:06:06 -0400

    Cleanup

--- app/routes/users.ts
+++ app/routes/users.ts
@@ -6,22 +6,21 @@ const router = express.Router();
 
 router.get('/:id', async function(req, res, next) {
   const id = req.params['id'];
-  
-  const resolve = (user: userModel.User) => {
-    if (!user) {
-      return res.status(404).json(null);
-    }
-    
-    return res.json({
-      name: user.name,
-      id: user.id,
-      time: (new Date()).toISOString()
-    });
-  };
-  
+
   try {
     let user = await userModel.getUser(id);
-    resolve(user);
+
+    if (!user) {
+      return res.status(404).json(null);
+
+    } else {
+      return res.json({
+        name: user.name,
+        id: user.id,
+        time: (new Date()).toISOString()
+      });
+    }
+    
   } catch (err) {
     next(err);
   }
